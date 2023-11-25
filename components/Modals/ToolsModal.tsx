5890354ff57cd3c7df41befb53fa9bcf6d2adb9e
Kshitij Srivastava <kshitij.suraj@gmail.com>
2023-11-25 22:02:41 +0530

    render: populated contents in tools modal

--- components/Modals/ToolsModal.tsx
+++ components/Modals/ToolsModal.tsx
@@ -7,16 +7,16 @@ interface Tool {
 export default function ToolsModal({ data }) {
   return Object.entries(data).map(([category, tools]) => (
     <div key={category}>
-      <h2>{category}</h2>
-      <ul>
+      <h2 className="pt-8 text-4xl md:pl-4">{category}</h2>
+      <ul className="w-full md:w-4/5">
         {(tools as Tool[]).map(({ name, subcategory }) => (
           <li
             key={name}
-            className="flex border-b border-gray-200 border-opacity-25 py-2 text-xl font-normal"
+            className="flex flex-col justify-between border-b border-gray-200 border-opacity-25 py-4 text-xl font-normal md:flex-row md:pl-4"
           >
-            {name}
+            <div>{name}</div>
 
-            <div>{subcategory}</div>
+            <div className="opacity-70 ">{subcategory}</div>
           </li>
         ))}
       </ul>
