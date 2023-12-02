de7320b9dc3fb67e732e25f43dd39305b0b76d94
Kshitij Srivastava <kshitij.suraj@gmail.com>
2023-12-02 21:22:01 +0530

    add: project images and links

--- components/Work/Work.tsx
+++ components/Work/Work.tsx
@@ -3,23 +3,27 @@ import Split from 'components/SplitText'
 export default function Work() {
   return (
     <div className="relative">
-      <div className="w-full px-12 mx-auto pb-28 md:w-4/5">
+      <div className="mx-auto w-full px-12 pb-28 md:w-4/5">
         <div className="relative z-10">
-          <h1 className="text-6xl font-bold tracking-wide split-lines md:text-8xl ">
+          <h1 className="split-lines text-6xl font-bold tracking-wide md:text-8xl ">
             <Split>Projects</Split>
           </h1>
         </div>
-        <div className="flex flex-col gap-10 mt-16 md:mt-28">
+        <div className="mt-16 flex flex-col gap-10 md:mt-28">
           <ProjectCard
             title="Github Readme Activity Graph"
             description="An open source project to show the last 30 days contributions of the github users on an animated graph."
-            imageUrl="/img/Second.jpg"
+            imageUrl="/img/activity-graph.png"
+            github="https://github.com/Ashutosh00710/github-readme-activity-graph"
+            live="https://ashutosh00710.github.io/github-readme-activity-graph/"
             number="01"
           />
           <ProjectCard
             title="Forkify"
             description="A recipe finder application using MVC architecture."
-            imageUrl="/img/Second.jpg"
+            imageUrl="/img/forkify.png"
+            github="https://github.com/Kshitij978"
+            live="https://kshitij978.github.io/Forkify/"
             number="02"
           />
         </div>
